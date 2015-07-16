3a990ef801061a6b1bafc3dd4bc48e37e05fc03f
GLZ92 <giovanniloizedda@gmail.com>
2015-07-16 15:42:56 +0200

    [RELEASE 18]

--- view/iscrizione.php
+++ view/iscrizione.php
@@ -1,44 +1,64 @@
-<html>
-    <head>
-        <title> Registrazione nuovo cliente </title>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    </head>
-    <body>
-        <p>Registrazione nuovo cliente</p>
-        <form id = "form" action ="index.php?arg=nuovoUtente" method = "POST">
-            <label>Username</label>
-            <input type ="text" id="username" name="username" required="required"/><br>
+<?php
+ echo  "<p>Registrazione nuovo cliente</p>
+        <form id = 'form' action ='index.php?arg=nuovoUtente' method = 'POST'>
+            <label>Nome</label>
+            <input type ='text' id='username' name='username' required='required'/><br>
             <label>Password</label>
-            <input type ="password" id="password" name="password" required="required"/><br>
+            <input type ='password' id='password' name='password' required='required'/><br>
             <label>Ripeti password</label>
-            <input type ="password" id="passwordR" name="passwordR" required="required" onChange="checkPasswords();"/><br>
-            <button type="submit" id="conferma" name="conferma" disabled>Conferma</button>
+            <input type ='password' id='passwordR' name='passwordR' required='required'/><br>
+            <button type='submit' id='conferma' name='conferma' disabled>Conferma</button>
         </form>
-        <div id="avviso"></div>
-
-        
-    </body>    
-</html>
-
-<script>
-    function checkPasswords() 
+		<div id='avviso'></div>";
+?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script type="text/javascript">
+$("#passwordR").keyup(function()
+{
+	var passwordR = $("#passwordR").val();
+    var password = $("#password").val();
+    if (password != passwordR)
     {
-        var passwordR = $("#passwordR").val();
-        var password = $("#password").val();
-
-        if (password != passwordR)
+	    $.get("view/nomatch.html", function(data)
         {
-            $("#avviso").html("Passwords don't match!");
-            $("#conferma").prop( "disabled", true); 
-        }
-        else
-        {
-            $("#avviso").html("Passwords match.");
-            $("#conferma").prop( "disabled", false); 
-        }
+	        $("#avviso").html(data);
+        })
+        $("#conferma").prop( "disabled", true);
     }
-
-    $(document).ready(function () {
-        $("#passwordR").keyup(checkPasswords);
-    });
+    else if(password != "" && passwordR != "")
+	{
+		$.get("view/match.html", function(data)
+        {
+        	$("#avviso").html(data);
+        })
+        $("#conferma").prop( "disabled", false);
+	}
+	else
+		$("#avviso").html("");
+});
+$("#password").keyup(function()
+{
+	var passwordR = $("#passwordR").val();
+    var password = $("#password").val();
+	
+    if (password != passwordR)
+    {
+	    $.get("view/nomatch.html", function(data)
+        {
+	        $("#avviso").html(data);
+        })
+        $("#conferma").prop( "disabled", true);
+    }
+    else if(password != "" && passwordR != "")
+	{
+		$.get("view/match.html", function(data)
+        {
+        	$("#avviso").html(data);
+        })
+        $("#conferma").prop( "disabled", false);
+	}
+	else
+            $("#avviso").html("");
+});
 </script>
+
